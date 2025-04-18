673ac380b9f9b9cb27ec1aa019b5d91cde549f40
Demesadaa <demetresadaa@gmail.com>
2025-04-18 07:48:54 +0400

    added test classes and completed simulator

--- src/GameSimulator.java
+++ src/GameSimulator.java
@@ -3,6 +3,7 @@ package utils;
 import model.Board;
 import model.Color;
 import model.Move;
+import model.pieces.Piece;
 import model.Position;
 
 import java.util.List;
@@ -38,7 +39,7 @@ public class GameSimulator {
             } else {
                 int moveNumber = (errorMoveIndex / 2) + 1;
                 String player = (errorMoveIndex % 2 == 0) ? "White" : "Black";
-                return String.format("Result: Invalid at %s's move %d ('%s'): %s",
+                return String.format("Result: Invalid at %s's move %d ('%s') - Reason: %s",
                         player, moveNumber, failedMoveString, message);
             }
         }
@@ -53,26 +54,62 @@ public class GameSimulator {
 
         for (int i = 0; i < moves.size(); i++) {
             Move currentMove = moves.get(i);
-            String moveString = "Invalid Move Data";
+            String moveNotation = "Invalid Move Data";
 
-            if (currentMove == null || currentMove.getStartPosition() == null) {
-                return new SimulationResult("Invalid Move object encountered (null or missing start position).", i, moveString);
+            if (currentMove == null || currentMove.getStartPosition() == null || currentMove.getEndPosition() == null) {
+                return new SimulationResult("Corrupted Move object encountered (null or missing positions).", i, moveNotation);
             }
+
             try {
-                moveString = currentMove.toString();
+                moveNotation = currentMove.toString();
             } catch (Exception e) {
+                System.err.println("Warning: Error creating string for move index " + i + ": " + e.getMessage());
             }
 
+            Color activePlayer = board.getCurrentPlayer();
+            Position originSquare = currentMove.getStartPosition();
+            Piece movingPiece = board.getPiece(originSquare);
 
-            boolean isLegal = true;
-            String failureReason = "";
-
-            if (!isLegal) {
-                String errorMessage = "Illegal move detected. Reason: " + failureReason;
-                return new SimulationResult(errorMessage, i, moveString);
+            if (movingPiece == null) {
+                return new SimulationResult("Illegal move: No piece located at start square " + originSquare, i, moveNotation);
             }
 
+            if (movingPiece.getColor() != activePlayer) {
+                return new SimulationResult(String.format("Illegal move: Piece at %s (%s) belongs to %s, but it's %s's turn",
+                        originSquare, movingPiece.getSymbol(), movingPiece.getColor(), activePlayer),
+                        i, moveNotation);
+            }
 
+            List<Move> possibleMoves;
+            try {
+                possibleMoves = board.generateLegalMoves();
+            } catch (Exception e) {
+                System.err.println("Exception generating legal moves at index " + i + ": " + e.getMessage());
+                e.printStackTrace();
+                return new SimulationResult("Internal error during legal move generation: " + e.getMessage(), i, moveNotation);
+            }
+
+            boolean moveIsAllowed = false;
+            for (Move legalOption : possibleMoves) {
+                if (Objects.equals(legalOption.getStartPosition(), currentMove.getStartPosition()) &&
+                        Objects.equals(legalOption.getEndPosition(), currentMove.getEndPosition()) &&
+                        legalOption.getPromotionChar() == currentMove.getPromotionChar()) {
+                    moveIsAllowed = true;
+                    break;
+                }
+            }
+
+            if (!moveIsAllowed) {
+                return new SimulationResult("Illegal move: The specified move is not permitted in the current position based on generated legal moves.", i, moveNotation);
+            }
+
+            try {
+                board.makeMove(currentMove);
+            } catch (Exception e) {
+                System.err.println("Exception executing move " + moveNotation + " at index " + i + ": " + e.getMessage());
+                e.printStackTrace();
+                return new SimulationResult("Internal error while applying validated move: " + e.getMessage(), i, moveNotation);
+            }
         }
 
         return new SimulationResult();
---
+++ src/test/model/pieces/BishopTst.java
@@ -0,0 +1,4 @@
+package test.model.pieces;
+
+public class BishopTst {
+}
